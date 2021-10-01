6bb0e537529ff83106c5f991f6837f4310ce8235
kelbach <87092340+Kelbach@users.noreply.github.com>
2021-10-01 18:14:21 -0400

    need uv colors and previous cities searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,57 @@
 var apiKey = "ec563e39065a30ccacdb51c199d52f0e";
 var searchBtn = $("#btn");
 
+var cities = JSON.parse(localStorage.getItem('cities')) || [];
+
+$("document").ready(function() {
+    for (var i = 0; i < cities.length; i++) {
+        var searched = $("<div>").addClass("col-12");
+        var button = $("<button city='"+cities[i]+"'>").text(cities[i]);
+        $("#searched").append(searched.append(button));
+    }
+});
+
+
+//SDKFHKFDHSLKFHDSFHDFSKHDSKLHDFJSHKLJFDSKLFHJDSKLHJDFSKLHSD//
+$("#city").on("click", function(event) {
+    event.preventDefault();
+    var city = $(this).val();
+    console.log(city);
+
+    // if (city) {
+    //     if(cities.indexOf(city) === -1) {
+    //         cities.push(city);
+    //         localStorage.setItem('cities', JSON.stringify(cities));
+    //         console.log(cities);
+    //         var searched = $("<div>").addClass("col-12");
+    //         var button = $("<button id='prev-btn'>").text(city);
+    //         $("#searched").append(searched.append(button));
+    //     }
+    //     $("#dash-title").text("Your Weather Dashboard for "+city);
+    //     getWeather(city);
+
+    // } else {
+    //     alert("Please Enter a City");
+    // }
+    
+});
+
+
+
+// var cityButtonHandler = function (event) {
+    
+//     var cityEl = event.target;
+//     var cityName = cityEl.textContent;
+//     getLatAndLon(cityName);
+// };
+// savedCityButtonsEl.addEventListener("click", cityButtonHandler);
+
+
 var displayWeather = function(data) {
+    $("#current").html("");
     console.log("..displaying weather..");
+    var date = moment.unix(data.dt).format("dddd, MMMM Do, YYYY");
+    console.log(date);
     var cWeatherCloud = data.weather[0].description;
     var cWeatherTemp = "Temp: "+ data.main.temp+"°F and "+cWeatherCloud;
     console.log(cWeatherTemp);
@@ -46,16 +95,116 @@ var displayWeather = function(data) {
     var cWeatherCloudIcon = data.weather[0].icon;
     
     //adds info to page. still need to add an icon
-    $('#current').html("<ul><li>"+cWeatherTemp+"</li><li>"+cWeatherWind+"</li><li>"+cWeatherHumid+"</li></ul>");
+    var cardTitle = $("<h3>").text(date);
+    var uList = $("<ul>").addClass("list-group-item");
+    var listTemp = $("<li id='temp'>").addClass("list-group-item").text(cWeatherTemp);
+    var listWind = $("<li>").addClass("list-group-item").text(cWeatherWind);
+    var listHumid = $("<li>").addClass("list-group-item").text(cWeatherHumid);
+    //var cardImage = $("<img src='http://openweathermap.org/img/wn/"+cWeatherCloudIcon+"@4x.png' />");
+
+    $('#current').append(cardTitle).append(uList.append(listTemp).append(listWind).append(listHumid));
+    //html("<h3>"+date+"</h3></li><li class='list-group-item'>"+cWeatherTemp+"</li><li class='list-group-item'>"+cWeatherWind+"</li><li class='list-group-item'>"+cWeatherHumid+"</li>");
     $('#image').html("<img src='http://openweathermap.org/img/wn/"+cWeatherCloudIcon+"@4x.png' />");
+
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+
+    getFive( lat , lon );
+};
+
+var getFive = function( x , y ) {
+    console.log("..getting future weather for lat: "+x+", lon: "+y);
+    var apiFCall = "https://api.openweathermap.org/data/2.5/onecall?lat="+x+"&lon="+y+"&exclude={"+"}&appid="+apiKey+"&units=imperial";
+    console.log(apiFCall);
+
+    fetch(apiFCall)
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                displayFive(data);
+            });
+        } else {
+            alert("Error: Five Day Forecast Not Found");
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to Open Weather");
+    });
+};
+
+var displayFive = function(data) {
+    $("#future").html("");
+
+    for (var i = 1; i < 6 ; i++) {
+        var day = data.daily[i];
+        var date = moment.unix(day.dt).format("dddd, Do");
+        console.log("/////////"+date+"/////////");
+
+        var fWeatherTemp = "Low: "+ day.temp.min+"°F, High: "+day.temp.max+"°F";
+        console.log(fWeatherTemp);
+
+        var fWeatherWindSpeed = day.wind_speed+" mph";
+        var fWeatherWindDeg = day.wind_deg;
+        var fWeatherWindDir = "";
+
+        //simple function for changing angle into cardinal direction
+        var windDirection = (function() {
+            if (0<fWeatherWindDeg<22.5 || 337.5<fWeatherWindDeg<360) {
+                fWeatherWindDir = "N";
+            } if (22.5<fWeatherWindDeg<67.5) {
+                fWeatherWindDir = "NE";
+            } if (67.5<fWeatherWindDeg<112.5) {
+                fWeatherWindDir = "E";
+            } if (112.5<fWeatherWindDeg<157.5) {
+                fWeatherWindDir = "SE";
+            } if (157.5<fWeatherWindDeg<202.5) {
+                fWeatherWindDir = "S";
+            } if (202.5<fWeatherWindDeg<247.5) {
+                fWeatherWindDir = "SW";
+            } if (247.5<fWeatherWindDeg<292.5) {
+                fWeatherWindDir = "W";
+            } if (292.5<fWeatherWindDeg<337.5) {
+                fWeatherWindDir = "NW";
+            }
+        });
+        windDirection();
+
+        var fWeatherWind = "Wind: "+fWeatherWindSpeed+" "+fWeatherWindDir;
+        console.log(fWeatherWind);
+
+        var fWeatherHumid = "Humidity: "+day.humidity+"%";
+        console.log(fWeatherHumid);
+
+        //image for sky
+        var fWeatherCloudIcon = day.weather[0].icon;
+
+        //generate cards for forecast
+        var card = $("<div>").addClass("card border m-1 w-40")
+        //.html("<h4>"+date+"</h4><ul><li class='list-group-item'>"+fWeatherTemp+"</li><li class='list-group-item'>"+fWeatherWind+"</li><li class='list-group-item'>"+fWeatherHumid+"</li></ul>");
+        var cardTitle = $("<h4>").text(date);
+        var uvF = Math.floor(day.uvi);
+        var uvFuture = $("<li>").addClass("list-group-item 'uv-"+uvF+"'").text("UV Index: "+ day.uvi);    
+        var ulist = $("<ul>").addClass("list-group");
+        var listTemp = $("<li id='temp'>").addClass("list-group-item").text(fWeatherTemp);
+        var listWind = $("<li>").addClass("list-group-item").text(fWeatherWind);
+        var listHumid = $("<li>").addClass("list-group-item").text(fWeatherHumid);
+        var cardImage = $("<img src='http://openweathermap.org/img/wn/"+fWeatherCloudIcon+"@4x.png' />");
+        
+        $('#future').append(card.append(cardTitle).append(cardImage).append(ulist.append(listTemp).append(listWind).append(listHumid).append(uvFuture)));
+    }
+    //generate uv index for current day
+    var uvF = Math.floor(data.current.uvi);
+    var uvCurrent = $("<li>").addClass("list-group-item 'uv-"+uvF+"'").text("UV Index: "+data.current.uvi);
+    $("#current ul").append(uvCurrent);
 };
 
 var getWeather = function(city) {
-    console.log("..getting weather..");
-    var apiCall = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey+"&units=imperial";
-    console.log(apiCall);
+    console.log("..getting current weather..");
+    var apiCCall = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey+"&units=imperial";
+    console.log(apiCCall);
 
-    fetch(apiCall)
+    fetch(apiCCall)
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
@@ -77,12 +226,23 @@ searchBtn.on("click", function(event) {
     console.log(city);
 
     if (city) {
+        if(cities.indexOf(city) === -1) {
+            cities.push(city);
+            localStorage.setItem('cities', JSON.stringify(cities));
+            console.log(cities);
+            var searched = $("<div>").addClass("col-12");
+            var button = $("<button id='prev-btn' city='"+city+"'>").text(city);
+            $("#searched").append(searched.append(button));
+        }
+        $("#dash-title").text("Your Weather Dashboard for "+city);
         getWeather(city);
+
     } else {
         alert("Please Enter a City");
     }
     
 });
+
 
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
 // WHEN I view current weather conditions for that city
